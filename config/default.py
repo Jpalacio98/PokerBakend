793cfa444167746481c963dba0302cf250894f54
Jpalacio98 <jdavidpalacio@unicesar.edu.co>
2025-08-22 15:48:37 -0500

    bd config

--- config/default.py
+++ config/default.py
@@ -2,16 +2,16 @@ import os
 
 class Config:
     # Configuración de la base de datos
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+pymysql://root:@localhost/poker')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://poker_1i73_user:uJDfmxjpjJHT0h3dKC9h6WTGRYaWiSYd@dpg-d2kdavali9vc73e7nj4g-a/poker_1i73')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Configuración de WebSocket
     SOCKETIO_MESSAGE_QUEUE = os.getenv('SOCKETIO_MESSAGE_QUEUE', 'redis://localhost:6379/0')
 
     # Configuración de la aplicación
-    SECRET_KEY = os.getenv('SECRET_KEY', 'tu_clave_secreta_aqui')
+    SECRET_KEY = os.getenv('SECRET_KEY', '^YXjotrXfbHX')
     DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
     # Configuración de JWT
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'tu_clave_secreta_jwt_aqui')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', '^YXjotrXfbHX')
     JWT_ACCESS_TOKEN_EXPIRES = int(os.getenv('JWT_ACCESS_TOKEN_EXPIRES', 3600))  # 1 hora
